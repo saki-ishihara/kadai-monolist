69885bc377586c029be578d9052035c166410218
saki.ishihara <ec2-user@ip-172-31-36-72.us-east-2.compute.internal>
2018-06-19 04:22:23 +0000

    have

---
+++ resources/views/items/items.blade.php
@@ -0,0 +1,29 @@
+@if ($items)
+    <div class="row">
+        @foreach ($items as $item)
+            <div class="item">
+                <div class="col-md-3 col-sm-4 col-xs-12">
+                    <div class="panel panel-default">
+                        <div class="panel-heading text-center">
+                            <img src="{{ $item->image_url }}" alt="">
+                        </div>
+                        <div class="panel-body">
+                            <p class="item-title"><a href="#">{{ $item->name }}</a></p>
+                           <div class="buttons text-center">
+                                @if (Auth::check())
+                                    @include('items.want_button', ['item' => $item])
+                                @endif
+                            </div>
+                            
+                              <div class="buttons text-center">
+                                @if (Auth::check())
+                                    @include('items.have_button', ['item' => $item])
+                                @endif
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        @endforeach
+    </div>
+@endif
