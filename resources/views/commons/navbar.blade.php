005dbf6898d3d97aeab2654adea81e5483678192
saki.ishihara <ec2-user@ip-172-31-36-72.us-east-2.compute.internal>
2018-06-19 06:39:08 +0000

    ranking

--- resources/views/commons/navbar.blade.php
+++ resources/views/commons/navbar.blade.php
@@ -12,12 +12,24 @@
             </div>
             <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
                 <ul class="nav navbar-nav navbar-right">
-                    @if (Auth::check())
+                      @if (Auth::check())
                         <li>
-                             <a href="{{ route('items.create') }}">
+                            <a href="#">
                                 <span class="glyphicon glyphicon-plus" aria-hidden="true"></span>
                                 アイテムを追加
                               </a>
+                        </li>
+
+                        <li class="dropdown">
+                            <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">
+                                <span class="glyphicon glyphicon-signal" aria-hidden="true"></span>
+                                ランキング
+                                <span class="caret"></span>
+                            </a>
+                            <ul class="dropdown-menu">
+                                <li><a href="{{ route('ranking.want') }}">Wantランキング</a></li>
+                                 <li><a href="{{ route('ranking.have') }}">Haveランキング</a></li>
+                            </ul>
                         </li>
 
                         <li class="dropdown">
@@ -39,11 +51,3 @@
                             </ul>
                         </li>
                     @else
-                        <li><a href="{{ route('signup.get') }}">新規登録</a></li>
-                        <li><a href="{{ route('login') }}">ログイン</a></li>
-                    @endif
-                </ul>
-            </div>
-        </div>
-    </nav>
-</header>
--- resources/views/items/items.blade.php
+++ resources/views/items/items.blade.php
@@ -1,23 +1,30 @@
 @if ($items)
     <div class="row">
-        @foreach ($items as $item)
+        @foreach ($items as $key => $item)
             <div class="item">
                 <div class="col-md-3 col-sm-4 col-xs-12">
                     <div class="panel panel-default">
                         <div class="panel-heading text-center">
-                            <img src="{{ $item->image_url }}" alt="">
+                            <img src="{{ $item->image_url }}" alt="" class="">
                         </div>
                         <div class="panel-body">
-                            <p class="item-title"><a href="#">{{ $item->name }}</a></p>
-                           <div class="buttons text-center">
+                            @if ($item->id)
+                                <p class="item-title"><a href="{{ route('items.show', $item->id) }}">{{ $item->name }}</a></p>
+                            @else
+                                <p class="item-title">{{ $item->name }}</p>
+                            @endif
+                            <div class="buttons text-center">
                                 @if (Auth::check())
                                     @include('items.want_button', ['item' => $item])
-                                @endif
-                                 @if (Auth::check())
                                     @include('items.have_button', ['item' => $item])
                                 @endif
                             </div>
                         </div>
+                        @if (isset($item->count))
+                            <div class="panel-footer">
+                                <p class="text-center">{{ $key+1 }}位: {{ $item->count}} Wants</p>
+                            </div>
+                        @endif
                     </div>
                 </div>
             </div>
